3e1c085cf58e62c48faa4a80d66fba50eca14f36
prashiddha <prashiddha@eyewa.com>
2021-08-11 21:42:27 +0400

    singly linked list

---
+++ singly_linkedlist.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type Node struct{
+    data string
+    next *Node
+}
+
+
+func (l *Node) append(newData string) *Node{
+    last := l
+
+    newNode := Node{
+        data: newData,
+        next: nil,
+    }
+
+    // linkedlist is empty
+    if (Node{} == *l) {
+        l = &newNode
+    }else{
+        for last.next != nil{
+            last = last.next
+        }
+        last.next = &newNode
+    }
+
+    return l
+}
+
+func (l *Node) display(){
+    last := l
+
+    if (Node{} == *last) {
+        fmt.Println("Linked List is Empty")
+        return
+    }
+
+    for ok := true; ok; ok = (last != nil) {
+        fmt.Println(last.data)
+        last = last.next
+    }
+
+}
+
+func main(){
+    list := &Node{}
+    list = list.append("6")
+    list = list.append("7")
+    list = list.append("8")
+    list = list.append("9")
+    list = list.append("10")
+    list = list.append("11")
+    list.display()
+    list = list.append("100")
+    list = list.append("110")
+    list.display()
+}
